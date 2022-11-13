f63ace578e2fca44e815979c03abe16b33149657
RikuDejima <riku.127.358@gmail.com>
2022-11-14 07:29:31 +0900

    add SearchRepositoryViewModel()

---
+++ lib/view_model/search_repository_view_model.dart
@@ -0,0 +1,24 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:search_github_repository/api/github_api_client.dart';
+import 'package:search_github_repository/model/github_repository.dart';
+
+
+final searchRepositoryViewModel =
+    Provider((ref) => SearchRepositoryScreenViewModel(ref));
+
+final githubRepositoryListState =
+    StateProvider<List<GithubRepository>>((ref) => []);
+final searchTextState = StateProvider<String>(((ref) => ''));
+
+
+class SearchRepositoryScreenViewModel {
+  final Ref ref;
+  SearchRepositoryScreenViewModel(this.ref);
+
+  Future<void> reserchRepository(String query) async {
+    final repositoryList =
+        await ref.read(searchRepositoryApiClient).getRepositories(query);
+
+    ref.read(githubRepositoryListState.notifier).state = repositoryList;
+  }
+}
